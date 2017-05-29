d9c248902b4ebfee5104c7af686fd4d99393cc3c
Mattias Stjernstedt <mstjernstedt@gmail.com>
2017-05-29 10:36:38 +0900

    started on destructbiles and dealing damage

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+	public int maxHealth;
+	public int health;
+
+	// Use this for initialization
+	void Start()
+	{
+		health = maxHealth;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void Damage(int damage)
+	{
+		health -= damage;
+
+	}
+}
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 	public float moveSpeed;
-	Transform baseCore;
+	public int damage;
+	public GameObject target;
+	Vector3 targetVector;
 	Vector3 direction;
 
 	// Use this for initialization
 	void Start()
 	{
-		baseCore = FindObjectOfType<Base>().transform;
-		direction = baseCore.position - transform.position;
+		targetVector = target.transform.position + new Vector3(0, 0.5f, 0);
+		direction = targetVector - transform.position;
 		direction = direction.normalized;
 	}
 
 	// Update is called once per frame
 	void FixedUpdate()
 	{
-		if (Vector3.Distance(transform.position, baseCore.position) > 1f)
+		if (Vector3.Distance(transform.position, targetVector) > 1f)
 		{
 			transform.Translate(direction * moveSpeed);
+		}
+		else
+		{
+			target.GetComponent<Destructible>().Damage(damage);
+			Destroy(gameObject);
 		}
 	}
 }
